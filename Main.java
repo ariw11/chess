c4215e978508c763d35423bccae9e785fcc8d6a5
ariw11 <100102205+ariw11@users.noreply.github.com>
2022-02-20 19:25:50 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class Main {
+    Piece[][] board = Piece.newBoard();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] board = new int[8][8];
     }
 }
