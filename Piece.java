5ef56e296b2cd9cadb087f7db90a3a26d747b0bd
ariw11 <100102205+ariw11@users.noreply.github.com>
2022-02-20 20:10:41 -0600

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -7,4 +7,10 @@ public class Piece {
         result[0] = {new Rook(1, 1), new Knight(2, 1), new Bishop(3, 1), new Queen(4, 1), new King(5, 1), new Bishop(6, 1), new Knight(7, 1), new Rook(8, 1)};
         result[7] = {new Rook(1, 8), new Knight(2, 8), new Bishop(3, 8), new Queen(4, 8), new King(5, 8), new Bishop(6, 8), new Knight(7, 8), new Rook(8, 8)};
     }
+    
+    public Piece(int r, int f, boolean c) {
+        rank = r;
+        file = f;
+        color = c;
+    }
 }
