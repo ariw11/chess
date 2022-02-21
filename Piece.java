1d982765f8e12bc19b79306453a179ccf6c1810c
ariw11 <100102205+ariw11@users.noreply.github.com>
2022-02-20 19:34:24 -0600

    Create Piece.java

---
+++ Piece.java
@@ -0,0 +1,9 @@
+public class Piece {
+    int rank;
+    int file;
+    public Piece[][] newBoard() {
+        Piece[][] result = new Piece[8][8];
+        result[0] = {new Rook(1, 1), new Knight(2, 1), new Bishop(3, 1), new Queen(4, 1), new King(5, 1), new Bishop(6, 1), new Knight(7, 1), new Rook(8, 1)};
+        result[7] = {new Rook(1, 8), new Knight(2, 8), new Bishop(3, 8), new Queen(4, 8), new King(5, 8), new Bishop(6, 8), new Knight(7, 8), new Rook(8, 8)};
+    }
+}
